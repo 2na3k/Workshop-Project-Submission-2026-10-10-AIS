9b8e076a1c6d8a785bccac154f8f960190826761
Racker-Hank <khanhtranmailbox@gmail.com>
2026-09-25 11:20:43 +0800

    fix: add docker compose and API doc

--- SystemCode/remy/api/src/api/features/plan/router.py
+++ SystemCode/remy/api/src/api/features/plan/router.py
@@ -6,8 +6,7 @@ from .service import PlanService
 router = APIRouter()
 
 
-# @router.post("", response_model=PlanResponse)
-@router.post("")
+@router.post("", response_model=PlanResponse)
 def create_plan(
         request: PlanRequest,
         service: PlanService = Depends(get_plan_service)
